375352994b4ddf86a7350048711b5828261ecd40
Kene QM <51497318+KeneQuirosM@users.noreply.github.com>
2024-09-05 08:35:23 -0600

    Update Database.cs

--- Way_Fit_Tickets/clases/Database.cs
+++ Way_Fit_Tickets/clases/Database.cs
@@ -13,8 +13,7 @@ namespace Way_Fit_Tickets
 
         public SqlConnection Connect_DB()
         {
-             Connection = new SqlConnection("Data Source=DESKTOP-4U3QFV7\\KENEQMMSSQLSVR;Initial Catalog=WayF_Tickets;User ID=sa;Password=Morrish0803");
-            //conexion = new SqlConnection("Data Source=DESKTOP-08DF72E;Initial Catalog=Prestamos;User ID=sa;Password=Morrish0803");
+             Connection = new SqlConnection("Data Source=DESKTOP-4U3QFV7\\KENEQMMSSQLSVR;Initial Catalog=WayF_Tickets;User ID=sa;Password=Pass");
             try
             {
                 Connection.Open();
